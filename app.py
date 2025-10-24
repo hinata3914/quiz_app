5580b466f6d8345588905748c6cf12ce1457fdfc
hinata3914 <hinata11033914@gmail.com>
2025-10-24 15:24:37 +0900

    関数作成・正誤判定絶対化

--- app.py
+++ app.py
@@ -2,42 +2,31 @@ from flask import Flask, render_template, request
 import random
 import pandas as pd
 import os
+from utils.read_csv import get_dish_data, get_dish_data_by_id
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    csv_path = os.path.join(app.root_path, "static", "data.csv")
-    quiz_df = pd.read_csv(csv_path)
-
-    quiz_df.columns = quiz_df.columns.str.strip()  # カラム名の空白を削除
-    quiz_df["imagepath"] = quiz_df["imagepath"].str.strip()  # 値の空白も削除
-
-    dish = quiz_df.sample(n=1).iloc[0]
-    
-     # 辞書化してテンプレートで使いやすくする
-    dish_data = {
-        "name": dish["name"],
-        "image": dish["imagepath"],
-        "calorie": dish["calorie"]
-    }
+    dish_data = get_dish_data(app)
 
     # 正解＋ダミーの選択肢を作成
-    choices = [dish["calorie"], 400, 700, 900]
+    choices = [dish_data["calorie"], 400, 700, 900]
     random.shuffle(choices)
     return render_template("index.html", dish=dish_data, choices=choices)
 
 
-@app.route("/check", methods=["POST"])
-def check():
-    selected = request.form["answer"]
-    correct = request.form["correct"]
-
-    if selected == correct:
+@app.route("/check/<int:dish_id>", methods=["POST"])
+def check(dish_id):
+    dish_data = get_dish_data_by_id(app, dish_id)
+    
+    selected = int(request.form["answer"])
+    
+    if selected == dish_data["calorie"]:
         result = "🎉 正解！"
     else:
-        result = f"❌ 不正解… 正解は {correct} kcal でした！"
+        result = f"❌ 不正解… 正解は {dish_data['calorie']} kcal でした！"
 
     return render_template("result.html", result=result)
 
